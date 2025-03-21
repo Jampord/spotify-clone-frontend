3d643edd2ee0555f55e77664ddafe71193b5558f
Jampord <cs.actubjohnford@gmail.com>
2025-03-21 15:05:02 +0800

    Implement routing with React Router; add HomePage, ChatPage, and authentication callback routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallBackPage from "./pages/auth-callback/AuthCallBackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <SignedOut>
-          <SignInButton>
-            <Button>Sign In</Button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </div>
+      <Routes>
+        <Route
+          path="/sso-callback"
+          element={<AuthenticateWithRedirectCallback signInForceRedirectUrl={"/auth-callback"} />}
+        />
+        <Route path="/auth-callback" element={<AuthCallBackPage />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
