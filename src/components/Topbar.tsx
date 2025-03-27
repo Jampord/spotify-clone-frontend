9b9c89e474a1dca4b64f9eff19a5aa8904bd9c61
Jampord <cs.actubjohnford@gmail.com>
2025-03-27 15:50:16 +0800

    Refactor Topbar to use useAuthStore for admin status and update UI elements

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,28 +1,35 @@
-import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react";
+import { SignedOut, UserButton } from "@clerk/clerk-react";
 import { LayoutDashboardIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import SignInOAuthButtons from "./SignInOAuthButtons";
+import { useAuthStore } from "@/stores/useAuthStore";
+import spotify from "../assets/spotify.png";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
 
 const Topbar = () => {
-  const isAdmin = false;
+  const { isAdmin } = useAuthStore();
+  console.log({ isAdmin });
+
   return (
     <div className="flex items-center justify-between sticky p-4 top-0 bg-zinc-900/75 backdrop-blur-md z-10">
-      <div className="flex gap-2 items-center">Spotify</div>
+      <div className="flex gap-2 items-center">
+        <img src={spotify} alt="spotify" className="size-8 shrink-0" />
+        Spotify
+      </div>
       <div className="flex gap-4 items-center">
         {isAdmin && (
-          <Link to="/admin">
+          <Link to="/admin" className={cn(buttonVariants({ variant: "outline" }))}>
             <LayoutDashboardIcon className="size-4 mr-2" />
             Admin Dashboard
           </Link>
         )}
 
-        <SignedIn>
-          <SignOutButton />
-        </SignedIn>
-
         <SignedOut>
           <SignInOAuthButtons />
         </SignedOut>
+
+        <UserButton />
       </div>
     </div>
   );
