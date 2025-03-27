549fb0eeceaa75694750dc6cbaf9d384a30d4bca
Jampord <cs.actubjohnford@gmail.com>
2025-03-27 15:50:24 +0800

    Add AudioPlayer component to MainLayout for audio playback management

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -2,12 +2,14 @@ import LeftSidebar from "@/layout/components/LeftSidebar";
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import { Outlet } from "react-router-dom";
 import FriendsActivity from "./components/FriendsActivity";
+import AudioPlayer from "@/components/AudioPlayer";
 
 const MainLayout = () => {
   const isMobile = false;
   return (
     <div className="h-screen bg-black text-white flex flex-col">
       <ResizablePanelGroup direction="horizontal" className="flex flex-1 h-full overflow-hidden p-2">
+        <AudioPlayer />
         {/*----left sidebar ----*/}
         <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
           <LeftSidebar />
