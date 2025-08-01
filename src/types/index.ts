45e6f9f429f153f9de52ab4eb681f5fa5e4a4049
Jampord <cs.actubjohnford@gmail.com>
2025-08-01 16:50:02 +0800

    feat: add Message and User interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -27,3 +27,19 @@ export interface Stats {
   totalArtists: number;
   totalUsers: number;
 }
+
+export interface Message {
+  _id: string;
+  senderId: string;
+  receiverId: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface User {
+  _id: string;
+  clerkId: string;
+  fullName: string;
+  imageUrl: string;
+}
